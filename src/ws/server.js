416a4cb2de9577357fdeb4a4ce702ed32ae6d044
V1R3J <virajbhatia1014@gmail.com>
2026-07-21 00:50:05 +0530

    implement cr suggested changes

--- src/ws/server.js
+++ src/ws/server.js
@@ -23,13 +23,25 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
     sendJson(socket, {
       type: "welcome",
-      message: "Welcome to the WebSocket server!",
     });
-    socket.on("error", console.error);
-  });
 
+    const interval = setInterval(() => {
+      wss.clients.forEach((ws) => {
+        if (ws.isAlive === false) return ws.terminate();
+        ws.isAlive = false;
+        ws.ping();
+      });
+    }, 30000); // Check every 30 seconds
+  });
+  wss.on("close", () => {
+    clearInterval(interval);
+  });
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: "match_created", data: match });
   }
